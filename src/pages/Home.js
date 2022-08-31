153a1eb2ef31ae93d41e82db3b78def4739dd17c
Sajid Mohammad <sajidmoha03@gmail.com>
2022-08-31 09:14:40 -0700

    created js files for all pages on the site

---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+import Header from "../components/Header";
+import Welcome from "../components/Welcome";
+
+function Home() {
+    return (
+        <div className="Home">
+            <Header></Header>
+            <Welcome></Welcome>
+        </div>
+    );
+}
+
+export default Home;
---
+++ src/pages/Store.js
@@ -0,0 +1,12 @@
+import Header from '../components/Header';
+
+function Store () {
+    return (
+        <div className="home">
+            <Header></Header>
+            hi
+        </div>
+    );
+}
+
+export default Store;
